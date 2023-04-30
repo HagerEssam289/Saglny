49fab01c9efcaa523acd431d968ef1c03a63276c
HagerEssam289 <127438476+HagerEssam289@users.noreply.github.com>
2023-04-30 12:44:07 +0200

    Add files via upload

---
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,377 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PnlRightSide = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Btn = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.PnlTopSide = new System.Windows.Forms.Panel();
+            this.pnlAddStudent = new System.Windows.Forms.Panel();
+            this.pnlRegister = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.lblCode = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.PnlRightSide.SuspendLayout();
+            this.pnlAddStudent.SuspendLayout();
+            this.pnlRegister.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PnlRightSide
+            // 
+            this.PnlRightSide.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.PnlRightSide.Controls.Add(this.label1);
+            this.PnlRightSide.Controls.Add(this.button4);
+            this.PnlRightSide.Controls.Add(this.panel4);
+            this.PnlRightSide.Controls.Add(this.button3);
+            this.PnlRightSide.Controls.Add(this.panel3);
+            this.PnlRightSide.Controls.Add(this.button1);
+            this.PnlRightSide.Controls.Add(this.panel1);
+            this.PnlRightSide.Controls.Add(this.Btn);
+            this.PnlRightSide.Controls.Add(this.panel2);
+            this.PnlRightSide.Dock = System.Windows.Forms.DockStyle.Right;
+            this.PnlRightSide.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.PnlRightSide.Location = new System.Drawing.Point(766, 0);
+            this.PnlRightSide.Name = "PnlRightSide";
+            this.PnlRightSide.Size = new System.Drawing.Size(207, 536);
+            this.PnlRightSide.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(21, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 49);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "     سجلني";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button4
+            // 
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button4.Location = new System.Drawing.Point(0, 278);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(194, 44);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "الترتيب";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button4.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Orange;
+            this.panel4.Location = new System.Drawing.Point(194, 278);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(12, 44);
+            this.panel4.TabIndex = 13;
+            // 
+            // button3
+            // 
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(0, 214);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(194, 44);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "تسجيل درجات";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Orange;
+            this.panel3.Location = new System.Drawing.Point(194, 214);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(12, 44);
+            this.panel3.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(0, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 44);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "تسجيل حضور";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Orange;
+            this.panel1.Location = new System.Drawing.Point(194, 151);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(12, 44);
+            this.panel1.TabIndex = 9;
+            // 
+            // Btn
+            // 
+            this.Btn.FlatAppearance.BorderSize = 0;
+            this.Btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Btn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Btn.Location = new System.Drawing.Point(0, 92);
+            this.Btn.Name = "Btn";
+            this.Btn.Size = new System.Drawing.Size(194, 44);
+            this.Btn.TabIndex = 8;
+            this.Btn.Text = "إضافه طالب";
+            this.Btn.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Btn.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.Btn.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Orange;
+            this.panel2.Location = new System.Drawing.Point(194, 92);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(12, 44);
+            this.panel2.TabIndex = 7;
+            // 
+            // PnlTopSide
+            // 
+            this.PnlTopSide.BackColor = System.Drawing.Color.Orange;
+            this.PnlTopSide.Location = new System.Drawing.Point(0, 0);
+            this.PnlTopSide.Name = "PnlTopSide";
+            this.PnlTopSide.Size = new System.Drawing.Size(973, 28);
+            this.PnlTopSide.TabIndex = 1;
+            // 
+            // pnlAddStudent
+            // 
+            this.pnlAddStudent.BackColor = System.Drawing.Color.Moccasin;
+            this.pnlAddStudent.Controls.Add(this.pnlRegister);
+            this.pnlAddStudent.Controls.Add(this.txtCode);
+            this.pnlAddStudent.Controls.Add(this.lblCode);
+            this.pnlAddStudent.Controls.Add(this.btnAdd);
+            this.pnlAddStudent.Controls.Add(this.txtName);
+            this.pnlAddStudent.Controls.Add(this.lblName);
+            this.pnlAddStudent.Location = new System.Drawing.Point(0, 28);
+            this.pnlAddStudent.Name = "pnlAddStudent";
+            this.pnlAddStudent.Size = new System.Drawing.Size(770, 508);
+            this.pnlAddStudent.TabIndex = 2;
+            // 
+            // pnlRegister
+            // 
+            this.pnlRegister.BackColor = System.Drawing.Color.Moccasin;
+            this.pnlRegister.Controls.Add(this.textBox1);
+            this.pnlRegister.Controls.Add(this.label2);
+            this.pnlRegister.Controls.Add(this.button2);
+            this.pnlRegister.Controls.Add(this.textBox2);
+            this.pnlRegister.Controls.Add(this.label3);
+            this.pnlRegister.Location = new System.Drawing.Point(0, 0);
+            this.pnlRegister.Name = "pnlRegister";
+            this.pnlRegister.Size = new System.Drawing.Size(770, 508);
+            this.pnlRegister.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append;
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(120, 159);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(368, 38);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label2.Location = new System.Drawing.Point(508, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 44);
+            this.label2.TabIndex = 5;
+            this.label2.Text = ": الكود";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.button2.Location = new System.Drawing.Point(141, 305);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(528, 53);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "إضافه";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append;
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(120, 80);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(368, 38);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label3.Location = new System.Drawing.Point(508, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 44);
+            this.label3.TabIndex = 0;
+            this.label3.Text = ": الأسم";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txtCode
+            // 
+            this.txtCode.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append;
+            this.txtCode.BackColor = System.Drawing.Color.White;
+            this.txtCode.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtCode.Location = new System.Drawing.Point(154, 186);
+            this.txtCode.Multiline = true;
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(368, 38);
+            this.txtCode.TabIndex = 6;
+            // 
+            // lblCode
+            // 
+            this.lblCode.Font = new System.Drawing.Font("Segoe UI Semibold", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblCode.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.lblCode.Location = new System.Drawing.Point(528, 186);
+            this.lblCode.Name = "lblCode";
+            this.lblCode.Size = new System.Drawing.Size(103, 44);
+            this.lblCode.TabIndex = 5;
+            this.lblCode.Text = ": الكود";
+            this.lblCode.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnAdd.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.btnAdd.Location = new System.Drawing.Point(141, 305);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(528, 53);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "إضافه";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // txtName
+            // 
+            this.txtName.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append;
+            this.txtName.BackColor = System.Drawing.Color.White;
+            this.txtName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtName.Location = new System.Drawing.Point(154, 103);
+            this.txtName.Multiline = true;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(368, 38);
+            this.txtName.TabIndex = 1;
+            // 
+            // lblName
+            // 
+            this.lblName.Font = new System.Drawing.Font("Segoe UI Semibold", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblName.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.lblName.Location = new System.Drawing.Point(528, 103);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(103, 44);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = ": الأسم";
+            this.lblName.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblName.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(973, 536);
+            this.Controls.Add(this.pnlAddStudent);
+            this.Controls.Add(this.PnlTopSide);
+            this.Controls.Add(this.PnlRightSide);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.PnlRightSide.ResumeLayout(false);
+            this.pnlAddStudent.ResumeLayout(false);
+            this.pnlAddStudent.PerformLayout();
+            this.pnlRegister.ResumeLayout(false);
+            this.pnlRegister.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PnlRightSide;
+        private System.Windows.Forms.Panel PnlTopSide;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button Btn;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel pnlAddStudent;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.Label lblCode;
+        private System.Windows.Forms.Panel pnlRegister;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
